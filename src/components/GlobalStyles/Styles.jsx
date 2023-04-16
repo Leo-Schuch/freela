e6ea3a4651fa93567319956b3e83b8e734f8a12f
DESKTOP-5SU2620\Usuario <leoschuch23@gmail.com>
2023-04-16 07:53:41 -0300

    added global styles with emotion. added font family

---
+++ src/components/GlobalStyles/Styles.jsx
@@ -0,0 +1,15 @@
+import { Global, css } from "@emotion/react"
+
+const styles = css`
+    * {
+        font-family: 'Montserrat', sans-serif;
+    }
+`
+
+export const Styles = () => {
+    return(
+        <Global styles={styles}
+        
+        />
+    )
+}
